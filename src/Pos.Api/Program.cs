c0fb08ffaba7f28d43be818ad7ce4a8328e4b53d
Junaid Munir <junaid.munir@epazz.com>
2026-01-28 07:45:36 +0500

    Added swagger in launchSettings

--- src/Pos.Api/Program.cs
+++ src/Pos.Api/Program.cs
@@ -17,23 +17,33 @@ builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 
+// Auth helpers
 builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+
+// Middleware DI
 builder.Services.AddScoped<SimpleIpRateLimitMiddleware>();
+
+builder.Services.AddControllers();
+
 
 
 var app = builder.Build();
 
+app.UseMiddleware<SimpleIpRateLimitMiddleware>();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 
-app.UseMiddleware<SimpleIpRateLimitMiddleware>();
-
-
 app.Run();
-
